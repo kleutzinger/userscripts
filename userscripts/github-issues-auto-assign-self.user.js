581c1341dba28b3e5e5ec2a281e668dbf8f1ad27
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2023-06-26 10:05:02 -0700

    run on all issues pages

--- userscripts/github-issues-auto-assign-self.user.js
+++ userscripts/github-issues-auto-assign-self.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        Github Issues: Auto Assign Self
 // @namespace   https://github.com/kleutzinger/userscripts
-// @match       https://github.com/*/*/issues/new
+// @match       https://github.com/*/*/issues/*
 // @downloadURL https://github.com/kleutzinger/userscripts/raw/main/userscripts/github-issues-auto-assign-self.user.js
 // @updateURL   https://github.com/kleutzinger/userscripts/raw/main/userscripts/github-issues-auto-assign-self.user.js
 // @grant       none
-// @version     0.1
+// @version     0.2
 // @author      github.com/kleutzinger/
 // @description Auto assign self when creating new issues on Github where possible.
 // @icon https://github.com/favicon.ico
