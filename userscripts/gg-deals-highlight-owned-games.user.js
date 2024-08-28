bb16592c03fcf6dd5352fa5cf5944fb4d3ea4668
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2024-08-28 14:40:13 -0400

    update gg.deals selectors

--- userscripts/gg-deals-highlight-owned-games.user.js
+++ userscripts/gg-deals-highlight-owned-games.user.js
@@ -5,7 +5,7 @@
 // @downloadURL https://github.com/kleutzinger/userscripts/raw/main/userscripts/gg-deals-highlight-owned-games.user.js
 // @updateURL   https://github.com/kleutzinger/userscripts/raw/main/userscripts/gg-deals-highlight-owned-games.user.js
 // @grant       none
-// @version     0.3
+// @version     0.4
 // @author      github.com/kleutzinger/
 // @description In lists of games on https://gg.deals, this highlights games you already have in your collection. To use, make an account on gg.deals and import your collection here https://gg.deals/collection/
 // @icon https://gg.deals/favicon.ico
@@ -13,25 +13,28 @@
 
 (function () {
   function apply() {
-    for (const e of new Set([
+    for (const wrapper of new Set([
       ...document.querySelectorAll(".game-info-wrapper"),
       ...document.querySelectorAll(".game-box-options"),
     ])) {
       // color owned and wishlisted games
       const selector_colors = [
-        { selector: "li.owned-game > span", color: "darkblue" },
-        { selector: "li.wishlisted-game > span", color: "darkred" },
+        { selector: ".owned-game .deactivate", color: "darkblue" },
+        { selector: ".wishlisted-game .deactivate", color: "darkred" },
       ];
       selector_colors.forEach(({ selector, color }) => {
-        const span = e.querySelector(selector);
+        const span = wrapper.querySelector(selector);
         const is_displayed = window.getComputedStyle(span).display !== "none";
         if (is_displayed) {
-          e.style.backgroundColor = color;
+          wrapper.style.backgroundColor = color;
+          // DEBUG
+          // console.log(`applying ${color} to`);
+          // console.log(e);
         }
       });
 
       // color positive reviews green
-      const rating_label = e.querySelector("span.reviews-label");
+      const rating_label = wrapper.querySelector("span.reviews-label");
       const rating = rating_label?.innerText.split("(")[0];
       if (!!rating) {
         if (rating.includes("Positive"))
