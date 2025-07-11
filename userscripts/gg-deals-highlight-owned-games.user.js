a47c8d98ef7331bede7e7be184523f941031f92c
Kevin Leutzinger <6435727+kleutzinger@users.noreply.github.com>
2025-07-11 09:55:04 -0400

    update blue

--- userscripts/gg-deals-highlight-owned-games.user.js
+++ userscripts/gg-deals-highlight-owned-games.user.js
@@ -5,7 +5,7 @@
 // @downloadURL https://github.com/kleutzinger/userscripts/raw/main/userscripts/gg-deals-highlight-owned-games.user.js
 // @updateURL   https://github.com/kleutzinger/userscripts/raw/main/userscripts/gg-deals-highlight-owned-games.user.js
 // @grant       none
-// @version     0.4
+// @version     0.5
 // @author      github.com/kleutzinger/
 // @description In lists of games on https://gg.deals, this highlights games you already have in your collection. To use, make an account on gg.deals and import your collection here https://gg.deals/collection/
 // @icon https://gg.deals/favicon.ico
@@ -19,7 +19,7 @@
     ])) {
       // color owned and wishlisted games
       const selector_colors = [
-        { selector: ".owned-game .deactivate", color: "darkblue" },
+        { selector: ".owned-game .deactivate", color: "#1b0ba1" },
         { selector: ".wishlisted-game .deactivate", color: "darkred" },
       ];
       selector_colors.forEach(({ selector, color }) => {
